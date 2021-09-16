2ded67d635f1e88da1dc4a6c75851342a755c49e
meiichin <meiichan3017@gmail.com>
2021-09-16 21:43:21 +0700

    [Added] assets [Added] section profile, name and status

--- FormNavigation/ContentView.swift
+++ FormNavigation/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                // Section Photo Profile
+                HStack{
+                    Image("me")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    //Name and status
+                    VStack(alignment:.leading){
+                        Text("Yuu Dev").font(.headline)
+                        Text("iOS Developer").font(.caption)
+                    }
+                }
+                .padding(.top,10)
+                .padding(.bottom,10)
+            }
+        }
     }
 }
 
