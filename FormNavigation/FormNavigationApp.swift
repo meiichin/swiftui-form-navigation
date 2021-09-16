102343880622d6dd9a39d1693c28a3ff6649822b
meiichin <meiichan3017@gmail.com>
2021-09-16 21:29:29 +0700

    Initial Commit

---
+++ FormNavigation/FormNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormNavigationApp.swift
+//  FormNavigation
+//
+//  Created by Yuu on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct FormNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
