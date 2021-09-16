70c6ca6929c0f09f143836e500691790a2102e19
meiichin <meiichan3017@gmail.com>
2021-09-16 22:03:20 +0700

    [Added] section general setting

--- FormNavigation/ContentView.swift
+++ FormNavigation/ContentView.swift
@@ -12,20 +12,45 @@ struct ContentView: View {
         NavigationView{
             Form{
                 // Section Photo Profile
-                HStack{
-                    Image("me")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    //Name and status
-                    VStack(alignment:.leading){
-                        Text("Yuu Dev").font(.headline)
-                        Text("iOS Developer").font(.caption)
+                Section(){
+                    HStack{
+                        Image("me")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        //Name and status
+                        VStack(alignment:.leading){
+                            Text("Yuu Dev").font(.headline)
+                            Text("iOS Developer").font(.caption)
+                        }
+                    }
+                    .padding(.top,10)
+                    .padding(.bottom,10)
+                }
+                
+                // Section general Setting
+                Section(header: Text("General Setting")){
+                    HStack(spacing: 20){
+                        Image(systemName: "star.fill")
+                            .frame(width: 35, height: 35)
+                            .background(Color.orange)
+                            .cornerRadius(10)
+                            .foregroundColor(Color.white)
+                        
+                        Text("Stared Messages")
+                    }
+                    
+                    HStack(spacing: 20){
+                        Image(systemName: "tv")
+                            .frame(width: 35, height: 35)
+                            .background(Color.green)
+                            .cornerRadius(10)
+                            .foregroundColor(Color.white)
+                        
+                        Text("Whats App Web/Desktop")
                     }
                 }
-                .padding(.top,10)
-                .padding(.bottom,10)
-            }
+            }.navigationBarTitle("Setting")
         }
     }
 }
